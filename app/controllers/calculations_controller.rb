530a20e8f466a5cb3dca07dd697bf55b3eae9ebe
Ryan Bertram <rpbertram@users.noreply.github.com>
2016-11-07 22:35:10 -0600

    set up general architecture. controllers not yet active

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,22 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    render("square.html.erb")
+  end
+
+  def square_root
+    render("square_root.html.erb")
+  end
+
+  def random
+    render("random.html.erb")
+  end
+
+  def payment
+    render("payment.html.erb")
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square/:NUMBER", { :controller => "calculations", :action => "square" })
+  get("/square_root/:NUMBER", { :controller => "calculations", :action => "square_root" })
+  get("/random/:MIN/:MAX", { :controller => "calculations", :action => "random" })
+  get("/payment/:INTEREST_RATE/:NUMBER_OF_YEARS/:PRINCIPAL_VALUE", { :controller => "calculations", :action => "payment" })
+
+
 end
